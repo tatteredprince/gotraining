ce41741aad33f8e8ed1373dba0215396d996f928
Tiserion Celtaris <tatteredprince@email.com>
2026-01-08 00:26:41 +0300

    add solution

--- two_complements/two_complements.go
+++ two_complements/two_complements.go
@@ -2,5 +2,13 @@ package main
 
 // hasTwoComplements indicates that complements contains two values which sums equals to target value
 func hasTwoComplements(target int, complements []int) bool {
+	set := map[int]struct{}{}
+	for _, v := range complements {
+		complement := target - v
+		if _, ok := set[complement]; ok == true {
+			return true
+		}
+		set[v] = struct{}{}
+	}
 	return false
 }

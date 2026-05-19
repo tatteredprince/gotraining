ed9c01659f3763bdfab92a61662eb45b451a7172
tatteredprince <tatteredprince@github.com>
2026-05-19 09:59:37 +0300

    add solution

--- concurrency_fibonacci_numbers_test.go
+++ concurrency_fibonacci_numbers_test.go
@@ -5,8 +5,8 @@ import (
 	"testing"
 )
 
-// channelsFibonacciNumbers returns channel that contains Fibonacci numbers
-func channelsFibonacciNumbers(first, second, count int) <-chan int {
+// generateFibonacciNumbers returns channel that contains Fibonacci numbers.
+func generateFibonacciNumbers(first, second, count int) <-chan int {
 	out := make(chan int)
 	go func() {
 		for range count {
@@ -18,11 +18,11 @@ func channelsFibonacciNumbers(first, second, count int) <-chan int {
 	return out
 }
 
-func channelsFibonacciNumbersTestHelper(t *testing.T, first, second, count int, expect []int) {
+func generateFibonacciNumbersTestHelper(t *testing.T, first, second, count int, expect []int) {
 	t.Helper()
 	t.Logf("calculate %d fibonacci numbers after %d and %d", count, first, second)
 	got := make([]int, 0, count)
-	for num := range channelsFibonacciNumbers(first, second, count) {
+	for num := range generateFibonacciNumbers(first, second, count) {
 		got = append(got, num)
 	}
 	if !slices.Equal(got, expect) {
@@ -30,14 +30,14 @@ func channelsFibonacciNumbersTestHelper(t *testing.T, first, second, count int, 
 	}
 }
 
-func TestChannelsFibonacciNumbers(t *testing.T) {
-	t.Run("Next", func(t *testing.T) { channelsFibonacciNumbersTestHelper(t, 0, 1, 1, []int{1}) })
-	t.Run("First five", func(t *testing.T) { channelsFibonacciNumbersTestHelper(t, 0, 1, 5, []int{1, 2, 3, 5, 8}) })
-	t.Run("Double digits", func(t *testing.T) { channelsFibonacciNumbersTestHelper(t, 5, 8, 5, []int{13, 21, 34, 55, 89}) })
+func TestGenerateFibonacciNumbers(t *testing.T) {
+	t.Run("Next", func(t *testing.T) { generateFibonacciNumbersTestHelper(t, 0, 1, 1, []int{1}) })
+	t.Run("First five", func(t *testing.T) { generateFibonacciNumbersTestHelper(t, 0, 1, 5, []int{1, 2, 3, 5, 8}) })
+	t.Run("Double digits", func(t *testing.T) { generateFibonacciNumbersTestHelper(t, 5, 8, 5, []int{13, 21, 34, 55, 89}) })
 	t.Run("Triple digits", func(t *testing.T) {
-		channelsFibonacciNumbersTestHelper(t, 55, 89, 5, []int{144, 233, 377, 610, 987})
+		generateFibonacciNumbersTestHelper(t, 55, 89, 5, []int{144, 233, 377, 610, 987})
 	})
 	t.Run("Big numbers", func(t *testing.T) {
-		channelsFibonacciNumbersTestHelper(t, 832040, 1346269, 10, []int{2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155, 165580141})
+		generateFibonacciNumbersTestHelper(t, 832040, 1346269, 10, []int{2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155, 165580141})
 	})
 }
